816169ecafa2b70d2786986264d6dd64d5eb7a2b
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-12-09 20:36:19 -0600

    initial functions for each page

---
+++ webpage/src/components/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function AboutPage(props) {
+  return (
+    <div>
+      <h1>New Component</h1>
+    </div>
+  )
+}
+ 
+export default AboutPage;
---
+++ webpage/src/components/HirePage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function HirePage(props) {
+  return (
+    <div>
+      <h1>New Component</h1>
+    </div>
+  )
+}
+ 
+export default HirePage;
