c6d3db6372c3fe9818198c1dca1c3300a52fd1ad
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2022-01-12 13:17:58 -0600

    fixing about grammar

--- webpage/src/components/AboutPage.jsx
+++ webpage/src/components/AboutPage.jsx
@@ -8,21 +8,21 @@ function AboutPage() {
         <div>
             <h1 className='about'>About Me</h1>
             <p className='textImg'> <img src={me} alt='Parker'></img>
-                Hello and welcome to my website. I am more than happy to tell you about myself and my coding journey. 
-                I graduated from Homestead High school, and had some prior knowledge of code from an elective course. 
-                I was introduced to Python and HTML in that course, and had many hardships 
-                understanding concepts that seemed inconceivable at the time. I told myself I’d never want a career writing code after taking the class. 
-                Thankfully, I didn’t hold myself to that promise . A passion for hardware, and fascination with software assumed my will to try again. 
-                I began using every resource available to me to learn online and teach myself. I took two Udemy courses for coding 2D and 3D VideoGames to start.
-                I moved onto reading the self-taught programmer by Cory Althoff, and Coding Games by Robert C Matthews. 
-                Although my main focus was to learn to make VideoGames through my journey of self-teaching I found myself constantly wanting to learn more. 
-                I wanted the ability to create anything, not just a video game. I enrolled in DevCodeCamp’s 12 week full-stack software development course. 
-                There I learned the fundamentals of JavaScript, C#, Python, HTML, and CSS. We learned tools and frameworks such as React.js, Node.js, Django, MySQL, .Net, and Redux. 
-                The learning was fast paced and the work was difficult, but was an incredible experience in the end. My knowledge, passion, and experience with code was greatly increased. 
-                I worked with some amazing developers and created functional full stack applications during my time at DevCodeCamp. 
-                Now I am working as a freelance developer while actively perusing my A+ and .Net certifications. 
-                I am grateful to myself most of all for not giving up because of the complexity. For me, creating software out of nothing is a beautiful, creative, and can be a life changing experience. 
-                Thank you for taking the time to read and get to know me! 
+                Hello and welcome to my website. I am excited to tell you about myself and my coding journey. 
+                I graduated Homestead High School, where I gained knowledge of code from an elective course. 
+                I was introduced to Python and HTML in that course, but I had trouble 
+                understanding coding concepts at the time. I thought I’d never want a career writing code after taking that class, but 
+                thankfully, I didn’t hold myself to that. A passion for hardware, and fascination with software inspired me to try again. 
+                I utilized every resource available to me to learn online and educate myself. First, I took two Udemy courses for coding 2D and 3D VideoGames.
+                Then I read the Self-Taught Programmer by Cory Althoff and Coding Games by Robert C Matthews. 
+                Although my main focus was to learn to make VideoGames, through my self-teaching journey, I found myself constantly wanting to learn more. 
+                I wanted the ability to create anything, not just a video game. I enrolled in DevCodeCamp’s 12-week full-stack software development course. 
+                There I learned the fundamentals of JavaScript, C#, Python, HTML, and CSS. I learned tools and frameworks such as React.js, Node.js, Django, MySQL, .Net, and Redux. 
+                The course was fast paced and the work was difficult, but it was an incredible experience in the end. My knowledge, passion, and experience with code was increased greatly. 
+                I worked with amazing developers and created functional full-stack applications during my time at DevCodeCamp. 
+                Now I am working as a freelance developer while actively pursuing my A+ and .Net certifications. 
+                I am proud of myself for not giving up because, for me creating software out of nothing is a beautiful, creative, and can be a life changing experience. 
+                Thank you for taking the time to read and get to know me. 
             </p>
             {/* <div className='img2'>
                 <img src={grad} alt='Parker'></img>
