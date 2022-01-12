0fd5fbc36de45b0d3dacc4a4014909480ff88f06
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2022-01-12 09:07:24 -0600

    mobile logo size

--- webpage/src/components/MediaPage.jsx
+++ webpage/src/components/MediaPage.jsx
@@ -6,14 +6,14 @@ function MediaPage() {
   return(
     <div class='media'>
       <ul>
-        <li class='list'>
+        <li class='item'>
           <span class='icon'>
             <a href="https://github.com/parkj56"><ion-icon name="logo-github"></ion-icon></a>
           </span>
           <span class='text'> Projects and Source Code </span>
         </li>
 
-        <li class='list'>
+        <li class='item'>
           <span class='icon'>
             <a href="https://www.linkedin.com/in/parker-nieves-129593208/"><ion-icon name="logo-linkedin"></ion-icon></a>
           </span>
@@ -21,7 +21,7 @@ function MediaPage() {
         </li>
 
 
-        <li class='list'>
+        <li class='item'>
           <span class='icon'>
             <a href="https://join.slack.com/t/slack-vzc9755/shared_invite/zt-11ctjgb3o-lIgBKgYnpGqcosv7FBZVZA"><ion-icon name="logo-slack"></ion-icon></a>
           </span>
